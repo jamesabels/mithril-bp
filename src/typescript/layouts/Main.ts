bd4f897701579559f5301b2b5b5162a035a6d59d
James Abels <jamesrabels@gmail.com>
2017-11-07 00:09:16 -0500

    Redo layout with sass grid

--- src/typescript/layouts/Main.ts
+++ src/typescript/layouts/Main.ts
@@ -2,13 +2,13 @@ import * as m from 'mithril';
 
 export default class MainLayout {
   view (vnode: any) {
-    return m("div", vnode.attrs, [
-      m('div', { class: 'sidebar-wrap'}, [
+    return m("div.row", vnode.attrs, [
+      m('div.sidebar-wrap.col-3', [
         m('div', {class: 'sidebar-content'}, [
           vnode.attrs.sidebar
         ])
       ]),
-      m('div', {class: 'main-wrap'}, [
+      m('div.main-wrap.col-9', [
         m('div', {class: 'main-content'}, [
           vnode.attrs.main,
           vnode.children

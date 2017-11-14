7fc47ce5252551755ea1272034b977c94456227d
James Abels <jamesrabels@gmail.com>
2017-11-13 23:54:01 -0500

    Add error toast to all of the app

--- src/typescript/layouts/Main.ts
+++ src/typescript/layouts/Main.ts
@@ -1,14 +1,43 @@
 import * as m from 'mithril';
 
+// Import components
+import ErrorToast from '../components/ErrorToast';
+
+// Import namespaces
+import { Api } from '../namespaces/Api';
+
 export default class MainLayout {
+  _renderError (verb: string, message: string) {
+    if (Api.state.error.length > 0) {
+      return m(ErrorToast, {verb, message})
+    }
+  }
+  oninit () {
+    Api.getUsers();
+  }
   view (vnode: any) {
-    return m("div.app.columns", vnode.attrs, [
-      vnode.attrs.sidebar,
-      m('div.main-wrap.column.col-9', [
-        m('div.main-content', [
-          vnode.attrs.main,
+    if (Api.state.error.length > 0) {
+      return m("div.app.columns", vnode.attrs, [
+        this._renderError(
+          Api.state.error[0].verb,
+          Api.state.error[0].message
+        ),
+        vnode.attrs.sidebar,
+        m('div.main-wrap.column.col-9', [
+          m('div.main-content', [
+            vnode.attrs.main,
+          ])
         ])
       ])
-    ])
+    } else {
+      return m("div.app.columns", vnode.attrs, [
+        vnode.attrs.sidebar,
+        m('div.main-wrap.column.col-9', [
+          m('div.main-content', [
+            vnode.attrs.main,
+          ])
+        ])
+      ])
+    }
   }
 }

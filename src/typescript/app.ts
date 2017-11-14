be2bcddf9100f32d0c3e344cad4ffb0074c4c8b2
James Abels <jamesrabels@gmail.com>
2017-11-13 23:08:19 -0500

    Add login page

--- src/typescript/app.ts
+++ src/typescript/app.ts
@@ -5,7 +5,13 @@ import { App } from './namespaces/App';
 
 // Import Pages
 import HomePage from './pages/Home';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+import SettingsPage from './pages/Settings';
 
 m.route(document.body, '/', {
-  '/': HomePage,
+  '/': LoginPage,
+  '/register': RegisterPage,
+  '/todo': HomePage,
+  '/settings': SettingsPage
 })
